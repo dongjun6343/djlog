f5f6bab691a6b45a0dfd20f59f9e522b8f24b4d4
dongjun6343 <dongjun6343@naver.com>
2023-05-23 17:00:58 +0900

    init

---
+++ src/main/java/com/dongjun/djlog/DjlogApplication.java
@@ -0,0 +1,13 @@
+package com.dongjun.djlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dongjun/djlog/DjlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongjun.djlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
